2b5b38a8df0b9970c976b261f8e0fd8a1da8e5f3
Demidenko Yuriy <dimidengo@yandex.ru>
2022-09-08 19:34:02 +0300

    Fixed scopes of properties and methods in MovieQuizPresenter

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -10,7 +10,7 @@ import UIKit
 final class MovieQuizPresenter {
 
     let questionsAmount = 10
-    var currentQuestion: QuizQuestion?
+    private var currentQuestion: QuizQuestion?
     private var rightAnswerCounter = 0
     private var currentQuestionIndex = 0
     private var statisticService: StatisticServiceProtocol!
@@ -28,20 +28,12 @@ final class MovieQuizPresenter {
 
     // MARK: - Internal functions
 
-    // Конвертируем модель вопроса во вью модель для состояния "Вопрос задан"
-    func convert(model: QuizQuestion) -> QuizStepViewModel {
-        let image = UIImage(data: model.image)
-        let question = model.text
-        let questionNumber = String(currentQuestionIndex + 1)
-        return QuizStepViewModel(image: image, question: question, questionNumber: questionNumber)
-    }
-
     // Проверяем правильность ответа
     func check(userAnswer: Bool) {
         guard let currentQuestion = currentQuestion else { return }
         if userAnswer == currentQuestion.correctAnswer {
             viewController?.showAnswerResult(isCorrect: true)
-            rightAnswerCounter += 1
+            rightAnswer()
         } else {
             viewController?.showAnswerResult(isCorrect: false)
         }
@@ -60,19 +52,15 @@ final class MovieQuizPresenter {
         questionFactory?.requestNextQuestion()
     }
 
-    func isLastQuestion() -> Bool {
-        currentQuestionIndex == questionsAmount - 1
-    }
-
-    func switchToNextQuestion() {
-        currentQuestionIndex += 1
-    }
-
-    func rightAnswer() {
-        rightAnswerCounter += 1
-    }
-
     // MARK: - Private functions
+
+    // Конвертируем модель вопроса во вью модель для состояния "Вопрос задан"
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let image = UIImage(data: model.image)
+        let question = model.text
+        let questionNumber = String(currentQuestionIndex + 1)
+        return QuizStepViewModel(image: image, question: question, questionNumber: questionNumber)
+    }
 
     // Проверка окончания игры
     private func showNextQuestionOrResult() {
@@ -106,6 +94,18 @@ final class MovieQuizPresenter {
                                        statisticService.gamesCount,
                                        bestQuizResult,
                                        statisticService.totalAccuracy)
+    }
+
+    private func isLastQuestion() -> Bool {
+        currentQuestionIndex == questionsAmount - 1
+    }
+
+    private func switchToNextQuestion() {
+        currentQuestionIndex += 1
+    }
+
+    private func rightAnswer() {
+        rightAnswerCounter += 1
     }
 }
 
